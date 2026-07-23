fe66d70a75b34f41a5f59c2fc105baed013a77c8
Rafo Bitro <67033048+rafobitro@users.noreply.github.com>
2026-07-23 14:06:05 -0700

    make backround movable

--- src/game.cpp
+++ src/game.cpp
@@ -4,8 +4,13 @@ float velocity;
 float gravity;
 Vector2 ballPosition;
 Texture2D background;
+int background_start;
+float background_speed;
+float delta_time;
 
 void init_game(int screenWidth, int screenHeight){
+    background_start = 0;
+    background_speed = 100;
     velocity = 0;
     gravity = 15;
     ballPosition = { (float)screenWidth/3, (float)screenHeight/2 };
@@ -21,12 +26,13 @@ void un_init_game(){
 }
 
 void run_phisics(){
-    float delta_time = GetFrameTime();
     velocity -= delta_time *  gravity * gravity  * 0.5;
     ballPosition.y -= velocity *delta_time;
 }
 
 void ubdate_game(){
+    background_start -= delta_time*background_speed;
+    if(background_start == -background.width) background_start = 0; 
     if (IsKeyPressed(KEY_SPACE)) velocity=120;
     run_phisics();
 }
@@ -40,11 +46,13 @@ void draw_game(){
 }
 
 void draw_background(){
-    DrawTexture(background,0,0,WHITE);
+    DrawTexture(background,background_start,0,WHITE);
+    DrawTexture(background,background_start+background.width,0,WHITE);
 }
 
 
 void run_game(){
+    delta_time=GetFrameTime();
     ubdate_game();
     draw_game();
 }
